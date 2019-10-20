f456cff5cc7cf2df454f39bd807fef7df1dd6022
Faizahhusniah <51088918+Faizahhusniah@users.noreply.github.com>
2019-10-20 11:14:37 +0700

    aaa

---
+++ accesmodifier.java
@@ -0,0 +1,11 @@
+public class accesmodifier {
+    public static void main(String[] args) {
+        accesmodifier accesObjek = new accesmodifier();
+
+    accesObjek.daspro();
+    }
+
+    private void daspro() {
+    }
+}
+
